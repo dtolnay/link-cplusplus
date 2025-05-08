0177c1995e8016b92384f1c22b8081f43fe0f502
elden <git@elden.anonaddy.com>
2025-05-08 22:10:42 +0300

    linux: fix linking libc++

--- build.rs
+++ build.rs
@@ -2,6 +2,35 @@ use std::env;
 use std::fs;
 use std::path::PathBuf;
 
+#[cfg(target_os = "linux")]
+fn linux_libcxx() -> bool {
+    let common_library_paths = vec![
+        PathBuf::from("/lib"),
+        PathBuf::from("/lib64"),
+        PathBuf::from("/usr/lib"),
+        PathBuf::from("/usr/lib64"),
+        PathBuf::from("/usr/local/lib"),
+    ];
+
+    for path in common_library_paths {
+        if !path.exists() {
+            continue;
+        }
+
+        if path.join("libc++.+so").exists() {
+            println!("cargo:rustc-link-lib=c++");
+            return true;
+        }
+
+        if path.join("libstc++.+so").exists() {
+            println!("cargo:rustc-link-lib=stdc++");
+            return true;
+        }
+    }
+
+    false
+}
+
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
 
@@ -25,13 +54,17 @@ fn main() {
         (false, true) => println!("cargo:rustc-link-lib=c++"),
         (false, false) | (true, true) => {
             // The platform's default.
-            let out_dir = env::var_os("OUT_DIR").expect("missing OUT_DIR");
-            let path = PathBuf::from(out_dir).join("dummy.cc");
-            fs::write(&path, "int rust_link_cplusplus;\n").unwrap();
-            cc::Build::new()
-                .cpp(true)
-                .file(&path)
-                .compile("link-cplusplus");
+            if cfg!(target_os = "linux") {
+                linux_libcxx();
+            } else {
+                let out_dir = env::var_os("OUT_DIR").expect("missing OUT_DIR");
+                let path = PathBuf::from(out_dir).join("dummy.cc");
+                fs::write(&path, "int rust_link_cplusplus;\n").unwrap();
+                cc::Build::new()
+                    .cpp(true)
+                    .file(&path)
+                    .compile("link-cplusplus");
+            }
         }
     }
 }
